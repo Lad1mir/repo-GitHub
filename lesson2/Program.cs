65b1076454b4b227a4975e0522701c08be662b14
Lad1mir <ladimir.matveev@gmail.com>
2021-07-26 01:25:57 +0300

    HomeWork2

---
+++ lesson2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1
+            /* Console.WriteLine("Введите минимальную температуру за сутки");
+            int minTemp = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            int maxTemp = int.Parse(Console.ReadLine());
+            double result = (minTemp + maxTemp) / 2;
+            Console.WriteLine($"Средняя температура за день =  {result}"); */
+
+            // Задание 2
+            /* Console.WriteLine("Введите номер текущего месяца");
+            int monthNum = int.Parse(Console.ReadLine());
+            switch (monthNum)
+            {
+                case 1:
+                    Console.WriteLine("Январь");
+                    break;
+                case 2:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3:
+                    Console.WriteLine("Март");
+                    break;
+                case 4:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5:
+                    Console.WriteLine("Май");
+                    break;
+                case 6:
+                    Console.WriteLine("Июнь");
+                    break;
+                case 7:
+                    Console.WriteLine("Июль");
+                    break;
+                case 8:
+                    Console.WriteLine("Август");
+                    break;
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12:
+                    Console.WriteLine("Декабрь");
+                    break;
+                default:
+                    Console.WriteLine("Такого месяца не существует");
+                    break;
+            } */
+
+            // Задание 3
+            /* Console.WriteLine("Введите любое число");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число не четное"); 
+            } */
+
+            // Задание 4
+            /* Console.WriteLine(
+                "Пятерочка\n" + 
+                "Добро Пожаловать!\n" +
+                "----------------\n" +
+                "Кассовый чек\n" +
+                "Картофель 1 x 50 =50\n" +
+                "Говядина 1кг 2 x 200 = 400\n" +
+                "Сыр Российский 1 x 150\n" +
+                "Итог = 600\n"
+
+            ); */
+            
+        }          
+
+                
+    }
+
+}
+
